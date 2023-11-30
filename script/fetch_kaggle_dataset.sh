1408df63b356d7249c5afe979a73a9f3ac9d906a
mikosa01 <michaelmerry8@gmail.com>
2023-11-30 11:21:56 +0000

    CI-Section

---
+++ script/fetch_kaggle_dataset.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash 
+
+kaggle competitions download -c playground-series-s3e24 -p
+package/classification/classification/dataset/
+
+
+
+unzip package/classification/classification/dataset/playground-series-s3e24.zip -d package/classification/classification/dataset
